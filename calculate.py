bfd65279881a4b6fe2a9e5df48b81c36f2fe8efa
ruboon-dej <ruboon_d@yahoo.com>
2021-02-04 00:16:27 +0700

    First commit

---
+++ calculate.py
@@ -0,0 +1,7 @@
+def calculate(total,function,number)
+    if function ==  "รับ":
+        total += number
+    elif function == "จ่าย":
+        total -= number
+    else:
+        total = "an error has occured please try again."
