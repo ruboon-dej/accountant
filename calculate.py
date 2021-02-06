3c05a1754d0c0f4c622b7a223a627d871cd5e4ed
ruboon-dej <ruboon_d@yahoo.com>
2021-02-06 16:43:42 +0700

    TRy

--- calculate.py
+++ calculate.py
@@ -1,9 +1,9 @@
 def calculate(total,function,number):
-    total = int(total)
+    print("working")
     if function ==  "รับ":
         total += number
     elif function == "จ่าย":
         total -= number
     else:
         total = "an error has occured please try again."
-    return str(total)
+    return total
