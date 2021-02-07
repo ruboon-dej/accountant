c266f677f0825d8630ce8a1f006764e8c886bc2b
ruboon-dej <ruboon_d@yahoo.com>
2021-02-07 20:40:15 +0700

    TRy

--- calculate_history.py
+++ calculate_history.py
@@ -1,10 +1,7 @@
 def calculate_history(history,action,function,number):
-    if below == "Number cannot be lower than 0":
-        pass
-    else:
-        if function == "รับ":
-            history += str(function) + " " + str(action) + " +" + str(number) + " บาท" + "\n"
-            return history
-        elif function == "จ่าย":
-            history += str(function) + " " + str(action) + " -" + str(number) + " บาท" + "\n"
-            return history
+    if function == "รับ":
+        history += str(function) + " " + str(action) + " +" + str(number) + " บาท" + "\n"
+        return history
+    elif function == "จ่าย":
+        history += str(function) + " " + str(action) + " -" + str(number) + " บาท" + "\n"
+        return history
