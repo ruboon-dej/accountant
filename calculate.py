3d3e4f58c6d3d0a3390718e7dedfd8774c9f5236
ruboon-dej <ruboon_d@yahoo.com>
2021-02-07 11:40:50 +0700

    TRy

--- calculate.py
+++ calculate.py
@@ -2,10 +2,9 @@ def calculate(total,function,number):
     if function ==  "รับ":
         total += number
     elif function == "จ่าย":
+        total -= number
         if number <= 0:
             raise Exception("Number cannot be lower than 0")
-        else:
-            total -= number
     else:
         raise Exception("Invalid Input")
     return total
